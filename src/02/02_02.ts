f07ee2f67956d8b1f0cb5f13d44f2d3469883b0d
Aleksandr_UB <bogdanov777000@mail.ru>
2022-06-02 17:49:59 +0300

    тесты JS - объекты

---
+++ src/02/02_02.ts
@@ -0,0 +1,24 @@
+//1
+export type StreetType = {
+    title: string
+}
+
+
+export type AdressType = {
+    number: number
+    street: StreetType
+}
+
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AdressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<string>
+    citizensNumber: number
+}
